5b34b85fbdf9d227106da3e4bf0e89fde928e7bf
benwest <ben.s.west@gmail.com>
2015-12-04 11:46:25 +0000

    Physics size correction

--- client/templates/anon/pages/about.js
+++ client/templates/anon/pages/about.js
@@ -10,108 +10,88 @@ Template.about.onRendered(function(){
 	$('.container').on('scroll.about', function(e){
 		if( $demoUniverse.offset().top < window.innerHeight / 2 ) Session.set(ANIMATION_START, true);
 	});
-
-
-
-
+	
 	Physics(function( world ) {
 
 		var viewWidth = window.innerWidth;
-		var viewHeight = window.innerHeight - (window.innerHeight/10);
-		var viewportBounds = Physics.aabb(0, 0, window.innerWidth, window.innerHeight);
+		var viewHeight = window.innerHeight * .9;
+		var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
 		var edgeBounce;
-		var colors = ['0xEDA989', '0xFFFFFF', '0xFFFFFF'];
-
-		var styles = {
-	      'circle': {
-	        fillStyle: colors[1],
-	        lineWidth: 0,
-	        strokeStyle: colors[0]
-	      }
-	      ,'rectangle': {
-	        fillStyle: colors[1],
-	      }
-	      ,'convex-polygon': {
-	        fillStyle: colors[0],
-	      }
-	    }
-
-
-		// Setting da renderer
+				
 		var renderer = Physics.renderer('pixi', {
-		    el: 'phyics-canvas', // id of the canvas element
+		    el: 'physics-canvas',
 		    width: viewWidth,
-		    height: viewHeight,
-		    styles: styles
+		    height: viewHeight
 		});
+		
+		renderer.container.style.width = renderer.el.style.width = viewWidth + 'px';
+		renderer.container.style.height = renderer.el.style.height = viewHeight + 'px';
+						
 		world.add( renderer );
 
-
-		var logos = ['/img/brands/logo_1.svg','/img/brands/logo_2.svg','/img/brands/logo_3.svg','/img/brands/logo_4.svg','/img/brands/logo_1.svg','/img/brands/logo_2.svg','/img/brands/logo_3.svg','/img/brands/logo_4.svg','/img/brands/logo_1.svg','/img/brands/logo_2.svg','/img/brands/logo_3.svg','/img/brands/logo_4.svg','/img/brands/logo_1.svg','/img/brands/logo_2.svg','/img/brands/logo_3.svg','/img/brands/logo_4.svg']
+		var logos = ['/img/brands/logo_1.svg'/*,'/img/brands/logo_2.svg','/img/brands/logo_3.svg','/img/brands/logo_4.svg','/img/brands/logo_1.svg','/img/brands/logo_2.svg','/img/brands/logo_3.svg','/img/brands/logo_4.svg','/img/brands/logo_1.svg','/img/brands/logo_2.svg','/img/brands/logo_3.svg','/img/brands/logo_4.svg','/img/brands/logo_1.svg','/img/brands/logo_2.svg','/img/brands/logo_3.svg','/img/brands/logo_4.svg'*/]
 		var spacing = viewWidth / logos.length;
-
-
-		var spheres = []
+		var size = 100 // Math.max(viewWidth, viewHeight) / (logos.length * 0.9);
+				
+		var circles = []
 		for(i = 0; i < logos.length; i++) {
-			sphere =  Physics.body('circle', {
+			var circle =  Physics.body('circle', {
 			  	x: spacing * i,
 			    y: 0,
 			    vx: -0.02,
-			    radius: 100
+			    radius: size
 			});
-
-			sphere.view = renderer.createDisplay('sprite', {
+			
+			circle.view = renderer.createDisplay('sprite', {
 				texture: logos[i],
 				anchor: {
 					x: 0.5,
 					y: 0.5
-				},
-				scale: {
-					x: 1,
-					y: 1
 				}
 			})
-
-			console.log(sphere.view);
-			spheres.push(sphere);
+			
+			circle.view.scale.x *= size / 100;
+			circle.view.scale.y *= size / 100;
+			
+			circles.push(circle);
 			
 		}
 
-		world.add( spheres );
-
+		world.add( circles );
 
 		world.render();
-
-
-		// Ticker 
+		
 		Physics.util.ticker.on(function( time, dt ){
 			world.step( time ); 
 		});
+		
 		Physics.util.ticker.start();
-
 
 		world.on('step', function(){
 			world.render();
 		});
-
+		
 		world.add( Physics.behavior('constant-acceleration') );
 
 		edgeBounce = Physics.behavior('edge-collision-detection', {
 			aabb: viewportBounds,
-			restitution: 0.3 // gradually slows momentum
+			restitution: 0.3
 		});
 
 		window.addEventListener('resize', function () {
-	   		viewportBounds = Physics.aabb(0, 0, renderer.width, renderer.height);
+			viewWidth = window.innerWidth;
+			viewHeight = window.innerHeight * .9;
+			renderer.container.style.width = renderer.el.style.width = viewWidth + 'px';
+			renderer.container.style.height = renderer.el.style.height = viewHeight + 'px';
+			viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
 	    	edgeBounce.setAABB(viewportBounds);
 		}, true);
-
-
+		
 		world.add( Physics.behavior('body-impulse-response'));
 		world.add( Physics.behavior('body-collision-detection'));
 		world.add( Physics.behavior('sweep-prune')); //optimisation
 		world.add( edgeBounce );
-	  	
+	  		  	
 		world.add(Physics.behavior('interactive', {
 			el: "physics-canvas"
 		}));
@@ -123,6 +103,7 @@ Template.about.onRendered(function(){
 
 Template.about.onDestroyed(function(){
 	$(window).off('.about');
+	Physics.util.ticker.stop();	
 })
 
 Template.about.helpers({
